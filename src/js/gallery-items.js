8140f7ef68ea0e7759329c0938443246cf0c1b81
S-Mariia <105200661+S-Mariia@users.noreply.github.com>
2022-11-03 15:45:49 +0200

    add pagination

---
+++ src/js/gallery-items.js
@@ -0,0 +1,37 @@
+// export const skillsItems = {
+//   // {
+//   //   preview:
+//   //     'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+//   //   original:
+//   //     'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+//   //   description: 'Hokkaido Flower',
+//   // },
+
+//   techSkills: [
+//     'HTML5',
+//     'Handlebars',
+//     'CSS3',
+//     'SASS',
+//     'Git version control',
+//     'Parcel',
+//     ' WebPack',
+//     'JavaScript',
+//     'React.js',
+//     'Node.js',
+//   ],
+
+//   softSkills: [
+//     'Communication (verbal and written)',
+//     'Teamwork and conflict resolution',
+//     'Empathy',
+//     'Adaptability',
+//     'Accountability',
+//     'Time management',
+//   ],
+
+//   languiges: [
+//     'English (pre intermediate)',
+//     'Polish (pre intermediate)',
+//     'Ukraine (native)',
+//   ],
+// };
